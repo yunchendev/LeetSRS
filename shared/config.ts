4db1cf69828ab882b8f75e4311109cff92e32a98
Matt Drake <mattdrakeworks@gmail.com>
2025-12-09 08:53:26 -0800

    chore: bump version

--- shared/config.ts
+++ shared/config.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.1.4';
+export const APP_VERSION = '0.1.5';
 export const CHROME_STORE_REVIEWS_URL =
   'https://chromewebstore.google.com/detail/leetsrs/odgfcigkohoimpeeooifjdglncggkgko/reviews?utm_source=item-share-cb';
